cdbc7dedec3d519e345ae741c8c3e0f9dbecfd1f
vytcka4 <vytautas.freelance@gmail.com>
2023-06-12 00:58:41 +0300

    commit

---
+++ lib/gradient_container.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:first_app/styled_text.dart';
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: [Color.fromRGBO(37, 150, 190, 1), Colors.white],
+        ),
+      ),
+      child: const Center(child: StyledText()),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:first_app/gradient_container.dart';
 
 void main() {
   runApp(
     MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-              colors: [Color.fromRGBO(37, 150, 190, 1), Colors.white],
-            ),
-          ),
-          child: const Center(
-            child: Text(
-              'Hello world',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 50,
-              ),
-            ),
-          ),
-        ),
-      ),
+      home: Scaffold(body: GradientContainer()),
     ),
   );
 }
-
-
-
-
-// class GradientContainer extends StatelessWidget {
-
-// @override
-
-// Widget build(context){
-
-//   return
-
-
-
-
-// }
-
-// }
