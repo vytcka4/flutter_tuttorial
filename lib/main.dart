c7bb2c7c30b9b1f54d77782eab1621c455d4b681
vytcka4 <vytautas.freelance@gmail.com>
2023-06-12 09:45:03 +0300

    commit

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [Color.fromRGBO(37, 150, 190, 1), Colors.white],
+            ),
+          ),
+          child: const Center(
+            child: Text(
+              'Hello world',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 50,
+              ),
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
+}
+
+// class GradientContainer extends StatelessWidget {
+// @override
+// Widget build(context){
+//   return 
+
+// }
+// }
